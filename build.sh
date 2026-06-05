1569224147d6b5eff04db7e53dd6a64f568eda06
Juanjo <jjpadillaguevara123@gmail.com>
2026-06-05 00:32:19 -0500

    crear superusuario admin

--- build.sh
+++ build.sh
@@ -3,3 +3,4 @@ set -o errexit
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
+python manage.py shell -c "from django.contrib.auth.models import User; User.objects.filter(username='admin').exists() or User.objects.create_superuser('admin','admin@cosfa.com','admin123')" 
