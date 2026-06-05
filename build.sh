7307a769270146ffbcabc19f7f8243800d734263
Juanjo <jjpadillaguevara123@gmail.com>
2026-06-05 00:44:15 -0500

    superusuario v3

--- build.sh
+++ build.sh
@@ -3,4 +3,5 @@ set -o errexit
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
-python manage.py shell -c "from django.contrib.auth.models import User; User.objects.filter(username='admin').exists() or User.objects.create_superuser('admin','admin@cosfa.com','admin123')" 
+python manage.py createsuperuser --noinput --username admin --email admin@cosfa.com || true
+python manage.py shell -c "from django.contrib.auth.models import User; u=User.objects.get(username='admin'); u.set_password('admin123'); u.save()" 
